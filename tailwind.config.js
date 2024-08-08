d982baaec337bec4a166311fa87173909e77542d
Fatma-M <fatmaamohamed941@gmail.com>
2024-08-08 12:30:10 +0300

    tailwind config, colors, and assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      screens: {
+        sm: "500px",
+        md: "700px",
+        lg: "900px",
+        xl: "1100px",
+      },
+    },
+    extend: {
+      colors: {
+        darkBlue: "#5964E0",
+        lightBlue: "#939BF4",
+        darkGray: "#6E8098",
+        darkerGray: "#D5D8F7",
+        lightGray: "#E8E8EA",
+        lighterGray: "#F4F6F8",
+        darkThemeBG: "#121721",
+        darkCardColor: "#19202D",
+        darkThemeGray: "#121721",
+      },
+    },
   },
   plugins: [],
 };
